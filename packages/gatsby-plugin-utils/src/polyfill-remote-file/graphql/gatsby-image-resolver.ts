7dd4d966618511fad4b7bfe43d4312a4ccd4b1b8
veryspry <ehlinger.matt@gmail.com>
2022-04-14 15:31:46 +0100

    feat: finish adding image cdn urls to redux

--- packages/gatsby-plugin-utils/src/polyfill-remote-file/graphql/gatsby-image-resolver.ts
+++ packages/gatsby-plugin-utils/src/polyfill-remote-file/graphql/gatsby-image-resolver.ts
@@ -239,7 +239,6 @@ export async function gatsbyImageResolver(
   }
 
   actions.processGatsbyImageSourceUrl(source.url)
-  // actions.processGatsbyImageSourceUrl(source.url)
 
   return {
     images: result as IGatsbyImageData,
